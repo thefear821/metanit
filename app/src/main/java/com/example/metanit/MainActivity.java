801a6cfe65c097db6d905af8df3caade30e621a6
thefear821 <sanay.ps@gmail.com>
2023-10-07 08:50:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/metanit/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.metanit;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final String AGE_KEY = "AGE";
+    static final String ACCESS_MESSAGE="ACCESS_MESSAGE";
+
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                    TextView textView = findViewById(R.id.textView);
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent intent = result.getData();
+                        String accessMessage = intent.getStringExtra(ACCESS_MESSAGE);
+                        textView.setText(accessMessage);
+                    }
+                    else{
+                        textView.setText("Ошибка доступа");
+                    }
+                }
+            });
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view) {
+        // получаем введенный возраст
+        EditText ageBox = findViewById(R.id.age);
+        String age = ageBox.getText().toString();
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(AGE_KEY, age);
+
+        mStartForResult.launch(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/metanit/User.java
@@ -0,0 +1,62 @@
+package com.example.metanit;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
+
+    private String name;
+    private String company;
+    private int age;
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel source) {
+            String name = source.readString();
+            String company = source.readString();
+            int age = source.readInt();
+            return new User(name, company, age);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    public User(String name, String company, int age){
+        this.name = name;
+        this.company = company;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCompany() {
+        return company;
+    }
+    public void setCompany(String company) {
+        this.company = company;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(company);
+        dest.writeInt(age);
+    }
+}
